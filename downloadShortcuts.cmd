fa7d712f1628a8bb8eb4a3f575efc29f8c63b6a6
samuelru3 <samuelruebel@gmail.com>
2021-12-19 23:39:30 +0100

    commit

---
+++ downloadShortcuts.cmd
@@ -0,0 +1,9 @@
+@echo off
+cd "C:\Program Files\Cleaning Tool\DesktopPreset"
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/AdobeAcrobatDC.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/Excel.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/Firefox.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/MicrosoftTeams.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/PowerPoint.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/Skype.lnk
+curl -LJO https://github.com/samuelru3/bereinigenToolDownload/raw/main/Word.lnk
